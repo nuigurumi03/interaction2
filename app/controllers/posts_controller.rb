49aec3ea21d1c89feb828d4068868de726130500
nuigurumi03 <nuigurumi03@gmail.com>
2020-02-02 10:15:31 +0900

    投稿画面の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,10 @@ class PostsController < ApplicationController
     @post.images.new
   end
 
-  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :detail, :prefecture_id, :city, :house_code, :build_name, images_attributes: [:image, :id]).merge(user_id:current_user.id)
+  end
 
 end
