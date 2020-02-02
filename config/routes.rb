080399075d588d81e16f395ef30d4048d6e81848
nuigurumi03 <nuigurumi03@gmail.com>
2020-02-02 09:29:53 +0900

    ユーザー登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'signup#index'
+  
+  resources :signup ,only: [:index] do
+    collection do
+      get 'registration'
+      post 'registration' => 'signup#first_validation'
+      get 'sms_authentication'
+      post 'sms_authentication' => 'signup#sms_check'
+      get 'address'
+      post 'address' => 'signup#second_validation'
+      get 'done'
+    end
+  end
+
 end
